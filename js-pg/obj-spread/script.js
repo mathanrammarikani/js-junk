b9895317791eca30b130a69e4ce841212cb41e9c
Mathan Ram <meetmemathanram@gmail.com>
2023-09-06 12:46:01 +0530

    constructing objects

--- js-pg/obj-spread/script.js
+++ js-pg/obj-spread/script.js
@@ -1,35 +1,10 @@
 let x;
 
-const todo = {};
+const todo = new Object();
 
 todo.id = 1;
 todo.name = "Buy milk";
 todo.completed = false;
 
 x = todo;
-
-const person = {
-  address: {
-    coords: {
-      lat: 42.9384,
-      lng: -71.3232,
-    },
-  },
-};
-
-const ob1 = {
-  a: 1,
-  b: 2,
-};
-const ob2 = {
-  c: 3,
-  d: 4,
-};
-
-const ob3 = {
-  ...ob1,
-  ...ob2,
-};
-
-x = ob3;
 console.log(x);
